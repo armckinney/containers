1a278f42cf8fe0ef96c248b26bc2fd4720145492
armck <armck.phantom@gmail.com>
2025-05-20 16:44:28 +0000

    updates for multiarch

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -14,17 +14,19 @@ helpFunction()
     echo -e "\t -i   Image name to build. Type: string."
     echo -e "\t -t   Image tag to build. Type: string."
     echo -e "\t -s   Image tag suffix to build. Type: string. Default: dev"
+    echo -e "\t -a   Flag for building multiarch images. Supports: linux/amd64,linux/arm64"
     echo -e "${nocolor}"
     exit 1
 }
 
 # acquiring opts, prints helpFunction in case parameter is non-existent
-while getopts "i:t:s:" opt
+while getopts "i:t:s:a" opt
 do
    case "$opt" in
         i ) image="$OPTARG" ;;
         t ) tag="$OPTARG" ;;
         s ) suffix="$OPTARG" ;;
+        a ) multiarch=1 ;;
         ? ) helpFunction ;;
    esac
 done
@@ -39,10 +41,19 @@ fi
 # variable override
 if [ -z "$suffix" ]; then suffix="dev"; fi
 
+# set platforms if multiarch flag is set
+if [ "$multiarch" = "1" ]; then
+    echo -e "${blue}Building multiarch image for $image:$tag-$suffix${nocolor}"
+    platforms="--platform linux/amd64,linux/arm64"
+else
+   echo -e "${blue}Building image for $image:$tag-$suffix${nocolor}"
+   platforms=""
+fi
+
 #####  SCRIPT  #####
 
 project_root=/workspaces/containers
 
-docker build -t "armck/$image:$tag-$suffix" \
+docker buildx build $platforms -t "armck/$image:$tag-$suffix" \
     -f "containers/$image/$tag/Dockerfile.$suffix" \
     $project_root
